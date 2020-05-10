e9cab070880ce6208dbc16e234aaf0a17ff9d73f
Julian Gallego <jcbest15@hotmail.com>
2020-05-09 22:23:45 -0500

    cambio de correo

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 
-app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// app.get('*', (request, response) => {
+//     response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
